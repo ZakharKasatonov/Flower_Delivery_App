eb0daa7c0aad180c7387aba1bc4807ab57a96010
Zakhar Kasatonov <zakharkasatonov@gmail.com>
2025-09-13 23:26:35 +0300

    change for the deploy

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,25 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD,
-  {
+let sequelize: Sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-  }
-);
+    protocol: "postgres",
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD,
+    {
+      dialect: "postgres",
+      host: process.env.DB_HOST!,
+      port: Number(process.env.DB_PORT),
+      logging: false,
+    }
+  );
+}
+
+export { sequelize };
